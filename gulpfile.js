617f7da35e588cc01d204d47033a8424c4874270
Francisco Ramos <jscriptcoder@gmail.com>
2014-04-16 22:22:04 +0200

    gulpfile configuration and added initial css

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp'),
     less = require('gulp-less');
 
 gulp.task('less', function () {
-
+    gulp.src('src/less/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('css/'));
 });
 
 gulp.task('default', function () {
-
+    gulp.watch('src/less/**/*.less', ['less']);
 });
